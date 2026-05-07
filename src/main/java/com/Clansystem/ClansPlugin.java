2b315e764628dd6ff6c272efad1cb7bdb22440a7
gamesove132 <gamesove132@gmail.com>
2026-05-07 17:24:42 +0300

    Create ClansPlugin.java

---
+++ src/main/java/com/Clansystem/ClansPlugin.java
@@ -0,0 +1,68 @@
+package com.clans;
+
+import com.clans.commands.ClansCommand;
+import com.clans.listeners.ClanListener;
+import com.clans.managers.ClanManager;
+import com.clans.managers.InviteManager;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ClansPlugin extends JavaPlugin {
+
+    private static ClansPlugin instance;
+    private ClanManager clanManager;
+    private InviteManager inviteManager;
+    private Economy economy;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+
+        clanManager = new ClanManager(this);
+        inviteManager = new InviteManager(this);
+
+        // Vault/XConomy інтеграція
+        if (getServer().getPluginManager().getPlugin("Vault") != null) {
+            RegisteredServiceProvider<Economy> rsp =
+                    getServer().getServicesManager().getRegistration(Economy.class);
+            if (rsp != null) {
+                economy = rsp.getProvider();
+                getLogger().info("Vault economy знайдено!");
+            }
+        } else {
+            getLogger().warning("Vault не знайдено — створення кланів без оплати!");
+        }
+
+        // Реєстрація команд
+        ClansCommand cmd = new ClansCommand(this);
+        getCommand("clans").setExecutor(cmd);
+        getCommand("clans").setTabCompleter(cmd);
+        getCommand("cc").setExecutor(cmd);
+        getCommand("cc").setTabCompleter(cmd);
+
+        // Listeners
+        getServer().getPluginManager().registerEvents(new ClanListener(this), this);
+
+        // PlaceholderAPI
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new com.clans.placeholders.ClansExpansion(this).register();
+            getLogger().info("PlaceholderAPI — плейсхолдери зареєстровано!");
+        }
+
+        getLogger().info("ClansPlugin увімкнено!");
+    }
+
+    @Override
+    public void onDisable() {
+        if (clanManager != null) clanManager.saveAll();
+        getLogger().info("ClansPlugin вимкнено!");
+    }
+
+    public static ClansPlugin getInstance() { return instance; }
+    public ClanManager getClanManager() { return clanManager; }
+    public InviteManager getInviteManager() { return inviteManager; }
+    public Economy getEconomy() { return economy; }
+}
