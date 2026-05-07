489deb1e125d7843d9b58f25863fc4638cba707a
gamesove132 <gamesove132@gmail.com>
2026-05-07 18:17:36 +0300

    Update ClanListener.java

--- src/main/java/com/Clansystem/Clanlistener/ClanListener.java
+++ src/main/java/com/Clansystem/Clanlistener/ClanListener.java
@@ -1,7 +1,7 @@
 package com.clans.listeners;
 
 import com.clans.ClansPlugin;
-import com.clans.model.Clan;
+import com.Clansystem.Model.Clan;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
